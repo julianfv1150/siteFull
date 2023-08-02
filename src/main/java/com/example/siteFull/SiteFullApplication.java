b74b8eadf32bb21315f57558162d3be0726039cb
julianfv1150 <julian.f.vega@gmail.com>
2023-08-02 16:52:25 -0300

    Inicio de proyecto JSB

---
+++ src/main/java/com/example/siteFull/SiteFullApplication.java
@@ -0,0 +1,13 @@
+package com.example.siteFull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteFullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/siteFull/SiteFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.siteFull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
